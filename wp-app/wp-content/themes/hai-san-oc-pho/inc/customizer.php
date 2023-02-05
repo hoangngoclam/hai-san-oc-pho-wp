9e7f4bf56cc952760fda787d424f9cf2f19d968f
truonghoangngoclam <truong.lam@marketenterprise.vn>
2023-02-05 19:09:40 +0700

    feat: add custom theem for hai-san-oc-pho (#1)

---
+++ wp-app/wp-content/themes/hai-san-oc-pho/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * hai-san-oc-pho Theme Customizer
+ *
+ * @package hai-san-oc-pho
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function hai_san_oc_pho_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'hai_san_oc_pho_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'hai_san_oc_pho_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'hai_san_oc_pho_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function hai_san_oc_pho_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function hai_san_oc_pho_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function hai_san_oc_pho_customize_preview_js() {
+	wp_enqueue_script( 'hai-san-oc-pho-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'hai_san_oc_pho_customize_preview_js' );
